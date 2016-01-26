cb1cb32ebae7f50b17810aaa5df4e898fba8a942
KaiDing <tracymacding@gmail.com>
2016-01-26 14:41:25 +0800

    remove lock from Find()

--- skip_list.go
+++ skip_list.go
@@ -1,9 +1,12 @@
 package skip_list
 
 import (
+	//"fmt"
 	"math/rand"
 	"sync"
+	"sync/atomic"
 	"time"
+	"unsafe"
 )
 
 const (
@@ -17,9 +20,9 @@ type Comparer interface {
 }
 
 type node struct {
-	value    interface{}
-	forwards []*node
-	// drawed   bool
+	value interface{}
+	// forwards []*node
+	forwards []unsafe.Pointer
 }
 
 type skipList struct {
@@ -33,28 +36,22 @@ type skipList struct {
 func NewNode(v interface{}, level int) *node {
 	n := new(node)
 	n.value = v
-	n.forwards = make([]*node, level)
+	// n.forwards = make([]*node, level)
+	n.forwards = make([]unsafe.Pointer, level)
 	for i := 0; i < level; i++ {
 		n.forwards[i] = nil
 	}
-	// n.drawed = false
 	return n
 }
 
-//func (n *node) DrawMysel() {
-//	if !n.drawed {
-//		for i := 0; i < n.level; i++ {
-//			println("|")
-//			println("|")
-//			print("---")
-//			println("|")
-//			println("|")
-//		}
-//	}
-//}
+func (n *node) SetNext(level int, n1 *node) {
+	atomic.StorePointer(&(n.forwards[level]), unsafe.Pointer(n1))
+	// n.forwards[level] = n1
+}
 
-func (n *node) LinkOther(other *node, level int, distance int) {
-
+func (n *node) Next(level int) *node {
+	// return n.forwards[level]
+	return (*node)(atomic.LoadPointer(&n.forwards[level]))
 }
 
 func NewSkipList(c Comparer) *skipList {
@@ -67,41 +64,62 @@ func NewSkipList(c Comparer) *skipList {
 	return s
 }
 
-// TODO:
-//func (s *skipList) Dump() {
-//	for i := s.level - 1; i >= 0; i-- {
-//		x := s.head
-//		for x := s.head; x != nil; x = x.forwards[i] {
-//			nodeDistance := s.distanceBetweenNode(x, x.forwards[i])
-//			x.DrawMysel()
-//			for i := 0; i < nodeDistance-1; i++ {
-//				print("-")
-//				print(" ")
-//			}
-//			print(">")
-//			x.forwards[i].DrawMysel()
-//			// 连接相邻两个节点
-//			x.LinkOther(x.forwards[i], i, nodeDistance)
-//		}
-//	}
-//}
+func (s *skipList) keyIsAfterNode(key interface{}, node *node) bool {
+	if node != nil && s.comparer.Less(node.value, key) {
+		return true
+	}
+	return false
+}
 
-func (s *skipList) Find(key interface{}) *node {
-	s.RLock()
-	defer s.RUnlock()
+func (s *skipList) currentLevel() int {
+	return s.level
+}
 
+func (s *skipList) findGreaterOrEqual(key interface{}, prev []*node) *node {
 	x := s.head
-	for i := s.level - 1; i >= 0; i-- {
+	level := s.currentLevel()
+
+	for i := level - 1; i >= 0; i-- {
 		for {
-			if x.forwards[i] != nil && s.comparer.Less(x.forwards[i].value, key) {
-				x = x.forwards[i]
+			next := x.Next(i)
+			if s.keyIsAfterNode(key, next) {
+				x = next
 			} else {
-				break
+				if prev != nil {
+					prev[i] = x
+				}
+				if i == 0 {
+					return next
+				} else {
+					break
+				}
 			}
 		}
 	}
-	if s.comparer.Equal(key, x.forwards[0].value) {
-		return x.forwards[0]
+	return nil
+}
+
+func (s *skipList) Find(key interface{}) *node {
+	//s.RLock()
+	//defer s.RUnlock()
+
+	//x := s.head
+	//for i := s.level - 1; i >= 0; i-- {
+	//	for {
+	//		if x.forwards[i] != nil && s.comparer.Less(x.forwards[i].value, key) {
+	//			x = x.forwards[i]
+	//		} else {
+	//			break
+	//		}
+	//	}
+	//}
+
+	n := s.findGreaterOrEqual(key, nil)
+	//if s.comparer.Equal(key, x.forwards[0].value) {
+	//	return x.forwards[0]
+	//}
+	if s.comparer.Equal(key, n.value) {
+		return n
 	}
 	return nil
 }
@@ -110,37 +128,43 @@ func (s *skipList) Insert(key interface{}) {
 	s.Lock()
 	defer s.Unlock()
 
-	update := make([]*node, maxLevel)
-	x := s.head
-	for i := maxLevel - 1; i >= 0; i-- {
-		for {
-			if x.forwards[i] != nil && s.comparer.Less(x.forwards[i].value, key) {
-				x = x.forwards[i]
-			} else {
-				break
-			}
-		}
-		update[i] = x
-	}
+	//update := make([]*node, maxLevel)
+	//x := s.head
+	//for i := maxLevel - 1; i >= 0; i-- {
+	//	for {
+	//		if x.forwards[i] != nil && s.comparer.Less(x.forwards[i].value, key) {
+	//			x = x.forwards[i]
+	//		} else {
+	//			break
+	//		}
+	//	}
+	//	update[i] = x
+	//}
+	prev := make([]*node, maxLevel)
+	n := s.findGreaterOrEqual(key, prev)
 
-	if x.forwards[0] != nil && s.comparer.Equal(key, x.forwards[0].value) {
-		x.forwards[0].value = key
+	//if x.forwards[0] != nil && s.comparer.Equal(key, x.forwards[0].value) {
+	//	x.forwards[0].value = key
+	//	return
+	//}
+	if n != nil && s.comparer.Equal(key, n.value) {
+		n.value = key
 		return
 	}
 
 	level := s.genRandomLevel()
-	n := NewNode(key, level)
+	n = NewNode(key, level)
 	if level > s.level {
-		//for i := s.level; i < level; i++ {
-		//	s.head.forwards[i] = n
-		//}
+		for i := s.currentLevel(); i < level; i++ {
+			prev[i] = s.head
+		}
 		s.level = level
 	}
 	for i := 0; i < level; i++ {
-		n.forwards[i] = update[i].forwards[i]
-		// fmt.Printf("node [%d], level [%d], forward:%v\n", n.value, i, update[i].forwards[i])
-		update[i].forwards[i] = n
-		// fmt.Printf("node [%d], level [%d], forward:%v\n", update[i].value, i, n.value)
+		n.SetNext(i, prev[i].Next(i))
+		// fmt.Printf("node [%d], level [%d], forward:%v\n", n.value, i, prev[i].Next(i))
+		prev[i].SetNext(i, n)
+		//fmt.Printf("node [%d], level [%d], forward:%+v\n", prev[i].value, i, n.value)
 	}
 }
 
@@ -148,34 +172,39 @@ func (s *skipList) Delete(key interface{}) {
 	s.Lock()
 	defer s.Unlock()
 
-	update := make([]*node, maxLevel)
-	x := s.head
-	for i := s.level - 1; i >= 0; i-- {
-		for {
-			if x.forwards[i] != nil && s.comparer.Less(x.forwards[i].value, key) {
-				x = x.forwards[i]
-				// } else if x.forwards[i] != nil && s.comparer.Equal(key, x.forwards[i].value) {
-				// update[i] = x
-				// break
-			} else {
-				break
-			}
-		}
-		update[i] = x
-		// fmt.Printf("update[%d]: %v\n", i, x)
-	}
-	if x.forwards[0] != nil && !s.comparer.Equal(x.forwards[0].value, key) {
+	//update := make([]*node, maxLevel)
+	//x := s.head
+	//for i := s.level - 1; i >= 0; i-- {
+	//	for {
+	//		if x.forwards[i] != nil && s.comparer.Less(x.forwards[i].value, key) {
+	//			x = x.forwards[i]
+	//		} else {
+	//			break
+	//		}
+	//	}
+	//	update[i] = x
+	//}
+	//if x.forwards[0] != nil && !s.comparer.Equal(x.forwards[0].value, key) {
+	//	return
+	//}
+	//n := x.forwards[0]
+	prev := make([]*node, maxLevel)
+	n := s.findGreaterOrEqual(key, prev)
+	if !s.comparer.Equal(n.value, key) {
 		return
 	}
-	n := x.forwards[0]
 
-	for i := 0; i < s.level; i++ {
-		if update[i].forwards[i] != nil && s.comparer.Equal(key, update[i].forwards[i].value) {
-			update[i].forwards[i] = n.forwards[i]
+	level := s.currentLevel()
+	for i := 0; i < level; i++ {
+		if prev[i] != nil && prev[i].Next(i) != nil && s.comparer.Equal(key, prev[i].Next(i).value) {
+			prev[i].SetNext(i, n.Next(i))
 		}
+		//if update[i].forwards[i] != nil && s.comparer.Equal(key, update[i].forwards[i].value) {
+		//	update[i].forwards[i] = n.forwards[i]
+		//}
 	}
 	for {
-		if s.level > 0 && s.head.forwards[s.level-1] == nil {
+		if s.level > 0 && s.head.Next(s.level-1) == nil {
 			s.level -= 1
 		} else {
 			break
